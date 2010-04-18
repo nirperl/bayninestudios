055ebb7f4b2755a621d13a09855e87f3d238025e
lee.davey <lee.davey@537026a8-1f4a-11df-b04c-09d9927b732b>
2010-04-18 17:10:04 +0000

    Initial commit, a basic opengl project

---
+++ trunk/ParticleSystemDemo/src/com/bayninestudios/particlesystemdemo/ParticleSystemDemo.java
@@ -0,0 +1,68 @@
+package com.bayninestudios.particlesystemdemo;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+
+public class ParticleSystemDemo extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mGLView = new ClearGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLView.onResume();
+    }
+
+    private GLSurfaceView mGLView;
+}
+
+class ClearGLSurfaceView extends GLSurfaceView {
+    public ClearGLSurfaceView(Context context) {
+        super(context);
+        mRenderer = new ClearRenderer();
+        setRenderer(mRenderer);
+    }
+
+    ClearRenderer mRenderer;
+}
+
+class ClearRenderer implements GLSurfaceView.Renderer
+{
+	public ClearRenderer()
+	{
+	}
+
+	public void onSurfaceCreated(GL10 gl, EGLConfig config)
+    {
+    }
+
+    public void onSurfaceChanged(GL10 gl, int w, int h)
+    {
+        gl.glViewport(0, 0, w, h);
+    }
+
+    public void onDrawFrame(GL10 gl)
+    {
+    	gl.glClearColor(0, 0, .5f, 1.0f);
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+    }
+}
