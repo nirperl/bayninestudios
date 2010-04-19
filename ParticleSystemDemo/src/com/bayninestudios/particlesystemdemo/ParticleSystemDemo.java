2fe58863ad5891946bcfd9ce24689fed1cfb4d13
lee.davey <lee.davey@537026a8-1f4a-11df-b04c-09d9927b732b>
2010-04-19 04:51:54 +0000

    Adding particles, systems and gl drawing

--- ParticleSystemDemo/src/com/bayninestudios/particlesystemdemo/ParticleSystemDemo.java
+++ ParticleSystemDemo/src/com/bayninestudios/particlesystemdemo/ParticleSystemDemo.java
@@ -7,13 +7,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
 import android.os.Bundle;
 
 public class ParticleSystemDemo extends Activity {
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         mGLView = new ClearGLSurfaceView(this);
@@ -45,24 +45,25 @@ class ClearGLSurfaceView extends GLSurfaceView {
     ClearRenderer mRenderer;
 }
 
-class ClearRenderer implements GLSurfaceView.Renderer
-{
-	public ClearRenderer()
-	{
+class ClearRenderer implements GLSurfaceView.Renderer {
+	private ParticleSystem mParticleSystem;
+	
+	public ClearRenderer() {
+		mParticleSystem = new ParticleSystem();
 	}
 
-	public void onSurfaceCreated(GL10 gl, EGLConfig config)
-    {
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        GLU.gluPerspective(gl, 15.0f, 80.0f/48.0f, 1, 100);
+        GLU.gluLookAt(gl, 0f, -10f, 15f, 0.5f, 0.0f, 0f, 0.0f, 1.0f, 1.0f);
     }
 
-    public void onSurfaceChanged(GL10 gl, int w, int h)
-    {
+    public void onSurfaceChanged(GL10 gl, int w, int h) {
         gl.glViewport(0, 0, w, h);
     }
 
-    public void onDrawFrame(GL10 gl)
-    {
+    public void onDrawFrame(GL10 gl) {
     	gl.glClearColor(0, 0, .5f, 1.0f);
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        mParticleSystem.draw(gl);
     }
 }
