acfa67b6de7e09151d5b2394d9bf192b64522508
lee.davey <lee.davey@537026a8-1f4a-11df-b04c-09d9927b732b>
2010-05-24 20:20:24 +0000

    Adding line drawing and using menu for interaction

--- trunk/AndroidBox2dDemo/src/com/bayninestudios/box2ddemo/Box2dDemo.java
+++ trunk/AndroidBox2dDemo/src/com/bayninestudios/box2ddemo/Box2dDemo.java
@@ -15,6 +15,7 @@ import org.jbox2d.dynamics.Body;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -25,6 +26,8 @@ import android.opengl.GLU;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.WindowManager;
 import android.widget.ToggleButton;
@@ -55,6 +58,35 @@ public class Box2dDemo extends Activity
         super.onResume();
         mGLView.onResume();
     }
+
+    /* Creates the menu items */
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        menu.add(Menu.NONE, 1, Menu.NONE, "Visit bayninestudios.com");
+        menu.add(Menu.NONE, 2, Menu.NONE, "Toggle Edit");
+        menu.add(Menu.NONE, 3, Menu.NONE, "Toggle Model");
+        return true;
+    }
+
+    /* Handles item selections */
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+        	case 2:
+        		((ClearGLSurfaceView) mGLView).toggleEdit();
+        		return true;
+        	case 3:
+        		((ClearGLSurfaceView) mGLView).toggleModel();
+        		return true;
+        	case 1:
+        		Intent myIntent = new Intent(Intent.ACTION_VIEW,
+        				android.net.Uri.parse("http://www.bayninestudios.com"));
+        		startActivity(myIntent);
+        		return true;
+        }
+        return false;
+    }
 }
 
 
@@ -68,6 +100,15 @@ class ClearGLSurfaceView extends GLSurfaceView
         setRenderer(mRenderer);
     }
 
+    public void toggleEdit()
+    {
+    	mRenderer.toggleEdit();
+    }
+
+    public void toggleModel()
+    {
+    	mRenderer.switchModel();
+    }
 //    public boolean onKeyUp(final int keyCode, KeyEvent msg) {
 //    	Log.d("key", "key event");
 //    	queueEvent(new Runnable(){
@@ -90,10 +131,7 @@ class ClearGLSurfaceView extends GLSurfaceView
     	queueEvent(new Runnable(){
             public void run()
             {
-            	if (event.getAction() == MotionEvent.ACTION_UP)
-            	{
-            		mRenderer.touchEvent(event.getX(), event.getY());
-            	}
+        		mRenderer.touchEvent(event.getX(), event.getY(), event.getAction());
             }});
         	// if this isn't here, multiple events happen
 	    	try {
@@ -112,17 +150,16 @@ class ClearRenderer implements GLSurfaceView.Renderer
 	private DrawModel mLongBox;
 	private DrawModel mCircle;
 	private int activeModel = 1;
+	private boolean editMode = false;
 
-	// TODO: shouldn't be here, should be in a config file or something
-	private float circleX = -5f;
-	private float circleY = -15f;
-	private float circleR = 5f;
 	private Context mContext;
 	SensorEventListener mSensorEventListener;
 	private List<Sensor> sensors;
 	private Sensor accSensor;
 	private int width;
 	private int height;
+	
+	private float startX, endX, startY, endY;
 	
 	public ClearRenderer(Context newContext, SensorManager sensorMgr)
 	{
@@ -186,9 +223,7 @@ class ClearRenderer implements GLSurfaceView.Renderer
         mWorld = new PhysicsWorld();
         mWorld.createWorld();
         mWorld.addBox(0f, -25f, 50f, 10f, 0f, false);
-        mWorld.addBox(-10f, 0f, 10f, .2f, -.2f, false);
-        mWorld.addBox(12f, -15f, 2f, 10f, 0f, false);
-        mWorld.addBall(circleX, circleY, circleR, false);
+        mWorld.addBall(-5f, -15f, 5f, false);
 
         mSensorEventListener = new SensorEventListener()
         {	
@@ -246,17 +281,37 @@ class ClearRenderer implements GLSurfaceView.Renderer
 
     public void onDrawFrame(GL10 gl)
     {
-    	gl.glClearColor(0, 0, .5f, 1.0f);
+    	if (editMode) {
+        	// draw a red background if edit mode
+    		gl.glClearColor(0.5f, 0, 0f, 1.0f);
+    	} else {
+    		// blue if not edit
+			gl.glClearColor(0f, 0, 0.5f, 1.0f);
+    	}
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
         gl.glTexEnvx(GL10.GL_TEXTURE_ENV, GL10.GL_TEXTURE_ENV_MODE,
             GL10.GL_REPLACE);
 
-        gl.glColor4f(1f, 1f, 1f, 1f);  // white
-    	drawActiveBody(gl);
-
+        if (!editMode)
+        {
+	    	drawActiveBody(gl);
+        }
     	gl.glColor4f(1f, 1f, 1f, 1f);  // white
     	Vec2 vec;
-        Body mBody = mWorld.getBodyList();
+
+    	
+    	if (editMode)
+    	{
+    		float midX = (startX+endX)/2f;
+    		float midY = (startY+endY)/2f;
+    		float sizeX = (endX - midX);
+    		float sizeY = (endY - midY);
+			float rotate = (float)Math.atan((double)(sizeY/sizeX));
+    		float size = (float)Math.sqrt((double)((sizeX*sizeX)+(sizeY*sizeY)));
+        	mBox.draw(gl, midX, midY, 0f, rotate*57.2957795f, size, .2f);
+    	}
+
+    	Body mBody = mWorld.getBodyList();
         do
         {
     		Shape mShape = mBody.getShapeList();
@@ -285,16 +340,40 @@ class ClearRenderer implements GLSurfaceView.Renderer
     public void switchModel()
     {
     	activeModel++;
-    	if (activeModel > 2)
+    	if (activeModel > 1)
     	{
     		activeModel = 0;
     	}
     }
 
+    // switches between which model gets dropped 
+    public void toggleEdit()
+    {
+    	if (editMode == false) {
+    		editMode = true;
+    	} else {
+    		editMode = false;
+    	}
+    }
+
+    public void addLine()
+    {
+		float midX = (startX+endX)/2f;
+		float midY = (startY+endY)/2f;
+		float sizeX = (endX - midX);
+		float sizeY = (endY - midY);
+		float rotate = (float)Math.atan((double)(sizeY/sizeX));
+		float size = (float)Math.sqrt((double)((sizeX*sizeX)+(sizeY*sizeY)));
+        mWorld.addBox(midX, midY, size, .2f, rotate, false);
+        startX = 0;
+        startY = 0;
+        endX = 0;
+        endY = 0;
+    }
     
     // interprets the touch event. Either switches models or
     // adds a model to the world.
-    public void touchEvent(float x, float y)
+    public void touchEvent(float x, float y, int eventCode)
     {
     	// look at all those magic numbers, truly magic!
     	// calculate world X and Y from the touch co-ords
@@ -303,20 +382,55 @@ class ClearRenderer implements GLSurfaceView.Renderer
 
     	// if the user clicks lower than the box at the bottom
     	// switch models
-    	if (worldY < -15f)
-    	{
-    		this.switchModel();
-    	}
-    	else
-    	{
-	    	if (activeModel == 0) {
-	    		mWorld.addBall(worldX, worldY, 0.98f, true);
-	    	} else if (activeModel == 1) {
-	        	mWorld.addBox(worldX, worldY, .98f, .98f, 0f, true);
-	    	} else if (activeModel == 2) {
-	        	mWorld.addBox(worldX, worldY, .2f, 2f, 0f, true);
-	    	}
-    	}
+//    	if (worldY < -15f)
+//    	{
+//    		if (eventCode == MotionEvent.ACTION_UP)
+//    		{
+//	    		if (worldX > 0f)
+//	    		{
+//	    			this.switchModel();
+//	    		}
+//	    		else
+//	    		{
+//	    			toggleEdit();
+//	    		}
+//    		}
+//    	}
+//    	else
+//    	{
+    		if (editMode == false)
+    		{
+    			if (eventCode == MotionEvent.ACTION_UP)
+    			{
+	    	    	switch (activeModel) {
+		        		case 0: mWorld.addBall(worldX, worldY, 0.98f, true); break;
+		        		case 1: mWorld.addBox(worldX, worldY, .98f, .98f, 0f, true); break;
+		        		case 2: mWorld.addBox(worldX, worldY, .2f, 2f, 0f, true); break;
+	    	    	}
+    			}
+    		}
+    		else // edit mode
+    		{
+    			if (eventCode == MotionEvent.ACTION_DOWN)
+    			{
+    				startX = worldX;
+    				startY = worldY;
+    				endX = worldX;
+    				endY = worldY;
+    			}
+    			else if (eventCode == MotionEvent.ACTION_MOVE)
+    			{
+    				endX = worldX;
+    				endY = worldY;
+    			}
+    			else if (eventCode == MotionEvent.ACTION_UP)
+    			{
+    				endX = worldX;
+    				endY = worldY;
+    				addLine();
+    			}
+    		}
+//    	}
     }
     
     // the easiest way to communicate to the renderer what the
